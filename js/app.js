d43db34e7f6cdf8554e2b0b7784686923044d4c7
cnickels21 <cnickels_21@yahoo.com>
2020-03-02 15:48:25 -0600

    basic html layout set up

--- js/app.js
+++ js/app.js
@@ -75,18 +75,6 @@ var lima = {
     }
 };
 
-// Within your javascript file (example: app.js), create separate JS object literals for each shop location that outputs the following to the sales.html file:
 
-// Stores the min/max hourly customers, and the average cookies per customer, in object properties
-// Uses a method of that object to generate a random number of customers per hour. Objects/Math/random
-// Calculate and store the simulated amounts of cookies purchased for each hour at each location using average cookies purchased and the random number of customers generated
-// Store the results for each location in a separate array… perhaps as a property of the object representing that location
 // Display the values of each array as unordered lists in the browser
-// Calculating the sum of these hourly totals; your output for each location should look like this:
 
-// Location	Min / Cust	Max / Cust	Avg Cookie / Sale
-// Seattle	23	65	6.3
-// Tokyo	3	24	1.2
-// Dubai	11	38	3.7
-// Paris	20	38	2.3
-// Lima	2	16	4.6
